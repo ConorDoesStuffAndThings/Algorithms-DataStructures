22c47ea607378db18c5ec790818b5ac0acc8b7b8
Conor H <conorhartigan13@gmail.com>
2020-01-27 14:22:43 +0000

    Added in more comments to QuestionTwo

--- Lab 1/QuestionTwo.java
+++ Lab 1/QuestionTwo.java
@@ -1,7 +1,4 @@
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class QuestionTwo {
     public static void main(String[] args) {
@@ -10,19 +7,22 @@ public class QuestionTwo {
         int[] counter = new int [100];
         String output="";
 
-
+        //for loop to end once i is less than the length of counter
         for(int i = 0; i < counter.length; i++){
             int number = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number"));
 
+            //error message if not between 1 and 100
             if(number>100){
                 JOptionPane.showMessageDialog(null, "Not a valid number, only numbers between 1 and 100 please");
 
             }
+            //if less than or equal to zero exit and break out of while loop
             else if(number <=0){
                 JOptionPane.showMessageDialog(null, "You have chosen to exit!");
                 break;
             }//end of if
 
+            //if number entered increment counter in array for that number
             else{
                 counter[number]++;
             }//end of else
@@ -34,11 +34,12 @@ public class QuestionTwo {
                 counter for 1 ++;
             }
          */
-
-        for(int i=0; i <= 99; i++){
+        //adds Sentence onto the String output and appends String
+        for(int i=0; i <= 100; i++){
+            //done this way to append in JOptionPane
             output += "The number of times " + i + " appeared is " + counter[i] +"\n";
 
-        }
+        }//end of for loop
 
         JOptionPane.showMessageDialog(null, output);
 
