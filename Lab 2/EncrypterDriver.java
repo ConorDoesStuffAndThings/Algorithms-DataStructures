820672ecb1b680681722457e1ed7cf8f2a2d6038
Conor H <conorhartigan13@gmail.com>
2020-01-30 15:50:46 +0000

    Add files via upload

---
+++ Lab 2/EncrypterDriver.java
@@ -0,0 +1,5 @@
+public class EncrypterDriver {
+    public static void main(String[] args) {
+        System.out.println(Encrypter.encrypt("rubber duckie is rubber but might be glue", 4));
+    }
+}
